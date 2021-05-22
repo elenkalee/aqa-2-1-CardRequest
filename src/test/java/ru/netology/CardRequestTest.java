d2727d79a6b083609bdd64fed3f5742a894d7aa0
elenkalee <elena.lipukhina@gmail.com>
2021-05-22 17:03:09 +0300

    update build.gradle and test

--- src/test/java/ru/netology/CardRequestTest.java
+++ src/test/java/ru/netology/CardRequestTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardRequestTest {
@@ -15,7 +17,7 @@ public class CardRequestTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+    WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
